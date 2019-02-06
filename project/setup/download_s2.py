e99cb34a44f207145db78991bbdbab0fd4074808
Joe Fennell <joseph.fennell@manchester.ac.uk>
2019-02-06 11:28:20 +0000

    updated scripts to generate training and validation

--- project/setup/download_s2.py
+++ project/setup/download_s2.py
@@ -5,6 +5,7 @@ import os
 os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
 #
 vpath = '../geo_files/cali_sugarcane.shp'
+vpath2 = '../geo_files/cali_validation.shp'
 
 query = gs_downloader.Query('S2', dt.datetime(2016,5,1).date(), dt.datetime.now().date(), vpath)
 query.product_details(proclevel='L1C', cloudcoverlimit=30)
@@ -14,10 +15,15 @@ nprods, prods = pipe.submit_query(query)
 
 #
 pipe.download_and_process(prods)
+gs_localmanager.add_tilelist()
 
 inventory = gs_localmanager.get_product_inventory()
 filtered = {uuid:prod for (uuid,prod) in inventory.items() if prod['platformname']=='Sentinel-2'}
 
-test_xr = gs_io.geofile_to_xarray(vpath,product_inventory=filtered)
+training = gs_io.geofile_to_xarray(vpath,product_inventory=filtered)
+training.to_netcdf(path='cali_training.nc',mode='w')
 
-test_xr.to_netcdf(path='cali_sugarcane.nc',mode='w')
+validation = gs_io.geofile_to_xarray(vpath2, product_inventory=filtered)
+validation.to_netcdf(path='cali_validation.nc',mode='w')
+
+print('Files Generated!')
